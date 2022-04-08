187543ec86a8c7d3967ee22172538b86611c0b48
Chao Wang <cclcwangchao@hotmail.com>
2022-04-08 17:05:23 +0800

    Add some flags

--- cases/tidb/staleread/stale_read_test.go
+++ cases/tidb/staleread/stale_read_test.go
@@ -15,9 +15,11 @@ var testTidbHost = flag.String("test-tidb-host", "127.0.0.1", "host of tidb")
 var testTidbPort = flag.Int("test-tidb-port", 4000, "port of tidb")
 var testTidbDatabase = flag.String("test-tidb-db", "test", "test database of tidb")
 var testTidbUser = flag.String("test-tidb-user", "root", "user of tidb")
-var testConcurrency = flag.Int("test-concurrency", 10, "test concurrency")
+var testStartIndex = flag.Int("test-start-index", 0, "test start index")
+var testLimit = flag.Int("test-limit", 0, "test limit")
 
 func TestStaleRead(t *testing.T) {
+	require.NoError(t, flag.Set("test.parallel", "1"))
 	forker, err := buildForker()
 	require.NoError(t, err)
 	iter, err := forker.DoFork(context.TODO())
@@ -29,33 +31,16 @@ func TestStaleRead(t *testing.T) {
 		require.NoError(t, iter.Next())
 	}
 
+	start := *testStartIndex
 	dsn := fmt.Sprintf("%s@tcp(%s:%d)/%s", *testTidbUser, *testTidbHost, *testTidbPort, *testTidbDatabase)
-	runOneCase := func(c *testCase, index int, parallel bool) {
+	for i, c := range cases {
+		if *testLimit > 0 && i >= *testLimit {
+			break
+		}
+
+		index := start + i
 		t.Run(fmt.Sprintf("%d/%d (%d actions)", index, len(cases), len(c.actions)), func(t *testing.T) {
-			if parallel {
-				t.Parallel()
-			}
 			c.Run(t, index, dsn)
 		})
-	}
-
-	batchCases := make([]*testCase, 0, *testConcurrency)
-	for i, c := range cases {
-		if *testConcurrency <= 1 {
-			runOneCase(c, i, false)
-			continue
-		}
-
-		batchCases = append(batchCases, c)
-		if len(batchCases) >= *testConcurrency {
-			start := i - len(batchCases) + 1
-			end := i
-			t.Run(fmt.Sprintf("from %d to %d", start, end), func(t *testing.T) {
-				for offset, cas := range batchCases {
-					runOneCase(cas, start+offset, true)
-				}
-			})
-			batchCases = batchCases[:0]
-		}
 	}
 }
